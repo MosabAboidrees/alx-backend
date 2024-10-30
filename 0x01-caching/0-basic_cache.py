367cb2ea144119b54dd20af52bcb720c242bdf1b
MOSAB ABOIDREES ALTRAIFI YOUSIF <33727845+MosabAboidrees@users.noreply.github.com>
2024-10-31 01:18:19 +0300

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ BasicCache module """
 
-from base_caching import BaseCaching
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
@@ -11,7 +12,6 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """ Add an item to the cache data dictionary.
-        
         Args:
             key: the key under which to store the item
             item: the item to be stored
@@ -21,10 +21,8 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """ Retrieve an item from the cache data dictionary by key.
-        
         Args:
             key: the key to look up in the cache data
-            
         Returns:
             The item associated with the key if it exists; None otherwise.
         """
